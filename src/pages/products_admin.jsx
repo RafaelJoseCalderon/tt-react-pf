582f3a734eec08fbb0a307e460367ae11cd1de7c
Rafael José <rafael_jose_calderon@hotmail.com>
2025-07-06 18:06:34 -0300

    confirmacion para eliminar

--- src/pages/products_admin.jsx
+++ src/pages/products_admin.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useProductsResources } from "../hooks/use_products_resourses";
 import { useProductsActions } from "../hooks/use_products_actions";
 
@@ -8,6 +9,7 @@ import ErrorsPromp from "../components/error_promp";
 import SearchBar from "../components/search_bar";
 import OverlaySpinner from "../components/overlay_spinner";
 import ProductListAdmin from "../components/product_list_admin";
+import ModalMessage from "../components/modal_Message";
 import Paginator from "../components/paginator";
 
 const ProductsAdmin = () => {
@@ -15,7 +17,19 @@ const ProductsAdmin = () => {
   const { query, pagination, actions } = paginatior;
   const { remove } = useProductsActions();
 
-  if (errors?.some(e => e !== null)) return <ErrorsPromp errors={errors} />;
+  const [confirm, setConfirm] = useState({ show: false, id: null });
+
+  const handleOpen = (id) => {
+    setConfirm({ show: true, id });
+  };
+  const handleClose = () => {
+    setConfirm({ show: false, id: null });
+  };
+  const handleConfirm = () => {
+    if (confirm.id !== null) remove(confirm.id);
+  };
+
+  if (errors?.some((e) => e !== null)) return <ErrorsPromp errors={errors} />;
 
   return (
     <Container className="my-5">
@@ -23,17 +37,25 @@ const ProductsAdmin = () => {
 
       <SearchBar query={query} loaded={loaded} onChange={actions.search} />
 
-      <Row><Col className="d-flex justify-content-end mb-2">
-        <Link className="btn btn-primary" to="/admin/create">Nuevo</Link>
-      </Col></Row>
+      <Row>
+        <Col className="d-flex justify-content-end mb-2">
+          <Link className="btn btn-primary" to="/admin/create">
+            Nuevo
+          </Link>
+        </Col>
+      </Row>
 
-      <Row><Col>
-        <OverlaySpinner loaded={loaded}>
-          <ProductListAdmin items={products} remove={(id) => { remove(id); }} />
-        </OverlaySpinner>
-      </Col></Row>
+      <Row>
+        <Col>
+          <OverlaySpinner loaded={loaded}>
+            <ProductListAdmin items={products} remove={handleOpen} />
+          </OverlaySpinner>
+        </Col>
+      </Row>
 
       {!loaded && <Paginator {...pagination} {...actions} />}
+
+      <ModalMessage show={confirm.show} onClose={handleClose} onConfirm={handleConfirm} />
     </Container>
   );
 };
